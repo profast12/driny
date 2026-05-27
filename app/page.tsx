8ace207144ad4dd02c2ec75cc501c897c844b33d
profast12 <joseloaiza481@gmail.com>
2026-05-26 22:45:53 -0500

    Agregar logo al navbar

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ const marcarLeidas = async () => {
 
       {/* NAVBAR PRINCIPAL */}
       <nav style={{
-        backgroundColor: '#b4b4b4',
+        backgroundColor: '#e9e9e9',
         padding: '14px 24px',
         display: 'flex',
         alignItems: 'center',
