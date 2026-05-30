65d3465b58ec2bd6c8d8691e20e4bf7f53be801a
profast12 <joseloaiza481@gmail.com>
2026-05-30 17:26:57 -0500

    Logo en vende con nosotros

--- app/subastas-panel/page.tsx
+++ app/subastas-panel/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
-import Image from "next/image";
 import { supabase } from "../../lib/supabase";
 
 const categorias = ["Electronica", "Ropa", "Hogar", "Deportes", "Juguetes", "Autos", "Arte", "Coleccionables", "Otro"];
